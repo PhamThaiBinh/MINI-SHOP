856a0a1251f7e67ef056e08fc0831bdc642d7445
PhamThaiBinh <binhpham.1512202@gmail.com>
2026-08-20 10:06:36 +0700

    fix: embed official Google Maps pb URL for Trung Tâm Tin Học Văn Phòng with centered red pin

--- src/app/(shop)/contact/page.tsx
+++ src/app/(shop)/contact/page.tsx
@@ -355,7 +355,7 @@ export default function ContactPage() {
             <div style={{ borderRadius: "1.25rem", overflow: "hidden", border: "1px solid #cbd5e1", height: "400px", background: "#f1f5f9", position: "relative" }}>
               <iframe
                 title="Bản đồ Trung Tâm Tin Học Văn Phòng - 107 D5 Phú Hòa"
-                src="https://maps.google.com/maps?q=Trung+T%C3%A2m+Tin+H%E1%BB%8Dc+V%C4%83n+Ph%C3%B2ng%2C+107+D5+Ph%C3%BA+H%C3%B2a+1%2C+Ph%C3%BA+L%E1%BB%A3i%2C+B%C3%ACnh+D%C6%B0%C6%A1ng&t=&z=16&ie=UTF8&output=embed"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.7231458921867!2d106.67841!3d10.97054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175290042a98e85%3A0x6b4fb712a20ca47e!2zVHJ1bmcgVMOibSBUaW4gSOG7jWMgVsSDbiBQaMOybmc!5e0!3m2!1svi!2s!4v1700000000000!5m2!1svi!2s"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
